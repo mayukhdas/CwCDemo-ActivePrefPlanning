2fd8a5084240161abbf1bd8eba238e918147fbbd
Mayukh Das <mayukhds@gmail.com>
2018-06-07 16:00:14 -0500

    Update to abstract Q

--- code/src/AdviceHandler/QueryTemplateGeneral.java
+++ code/src/AdviceHandler/QueryTemplateGeneral.java
@@ -1,5 +1,15 @@
 package AdviceHandler;
 
-public abstract class QueryTemplateGeneral {
+import java.util.Hashtable;
 
+import JSHOP2.TaskList;
+
+public abstract class QueryTemplateGeneral {
+	String task; 
+	Hashtable<String,TaskList[]> methodDesc;
+	public QueryTemplateGeneral(String task, Hashtable<String,TaskList[]> Desc)
+	{
+		this.task = task;
+		this.methodDesc = Desc;
+	}
 }
