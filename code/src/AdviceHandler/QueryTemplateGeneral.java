2e325fec7bb11582d59b690fcd899ab2679e55e7
Mayukh Das <mayukhds@gmail.com>
2018-06-08 13:45:40 -0500

    Template updates

--- code/src/AdviceHandler/QueryTemplateGeneral.java
+++ code/src/AdviceHandler/QueryTemplateGeneral.java
@@ -1,15 +1,36 @@
 package AdviceHandler;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Hashtable;
+import java.util.Set;
 
 import JSHOP2.TaskList;
 
 public abstract class QueryTemplateGeneral {
 	String task; 
-	Hashtable<String,TaskList[]> methodDesc;
-	public QueryTemplateGeneral(String task, Hashtable<String,TaskList[]> Desc)
+	ArrayList<String> methodDesc;
+	public QueryTemplateGeneral(String task, ArrayList<String> Desc)
 	{
 		this.task = task;
 		this.methodDesc = Desc;
+	}
+	
+	public String gettask()
+	{
+		String finalString = "Trying to "+this.task;
+		return finalString;
+	}
+	public String getMethods()
+	{
+		Collections.shuffle(this.methodDesc);
+		String out="";
+		int counter = 1;
+		for(String m:this.methodDesc)
+		{
+			out += counter +": "+m+"\n";
+			counter++;
+		}
+		return out;
 	}
 }
---
+++ code/src/blocks/QueryTemplateBlocks.java
@@ -0,0 +1,21 @@
+package blocks;
+
+import java.util.ArrayList;
+
+import AdviceHandler.QueryTemplateGeneral;
+
+public class QueryTemplateBlocks extends QueryTemplateGeneral {
+
+	public QueryTemplateBlocks(String task, ArrayList<String> Desc) {
+		super(task, Desc);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public static QueryTemplateBlocks getTemplate()
+	{
+		return null;
+	}
+	
+
+}
