e62c318e3e103d6be0e110aeac90ebeb54dab975
Mayukh Das <mayukhds@gmail.com>
2018-06-10 04:34:56 -0500

    Update template

--- code/src/blocks/QueryTemplateBlocks.java
+++ code/src/blocks/QueryTemplateBlocks.java
@@ -2,6 +2,7 @@ package blocks;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Random;
 
 import AdviceHandler.QueryTemplateGeneral;
 import JSHOP2.Method;
@@ -15,23 +16,38 @@ public class QueryTemplateBlocks extends QueryTemplateGeneral {
 		// TODO Auto-generated constructor stub
 	}
 	
-	
+	static String[] movement = {" from its current position to to the top of block ", " and place it on "," from its current stack and place it above "};
+	static Random r = new Random();
 	public static QueryTemplateBlocks getTemplate(TaskAtom task, Method[] methods)
 	{
+		String taskstring = "";
+		
 		if(task.getHead().isGround())
 		{
 			System.out.println("TERMS^^^^^^^^^^^^^^ "+task.getHead().getParam().toString());
-			String[] objs = task.getHead().getParam().toString().replaceAll("\\(", "").replaceAll("\\)", "").split(" ");
+			String[] objs = task.getHead().toString().replaceAll("\\(", "").replaceAll("\\)", "").split(" ");
+			if(objs[0].contains("move"))
+			{
+				int pick = r.nextInt(3);
+				taskstring = "move block "+objs[1]+movement[pick]+objs[2];
+			}
+			
 			for(int i=0;i<methods.length;i++)
 			{
 				System.out.println("Method TERMS^^^^^^^^^^^^^^ "+methods[i].getHead().getParam());
 				String[] vars = methods[i].getHead().getParam().toString().replaceAll("\\(", "").replaceAll("\\)", "").split(" ");
 				for(int j=0;j<vars.length;j++)
 				{
-					varMapping.put(vars[j], objs[j]);
+					varMapping.put(vars[j], objs[j+1]);
 				}
 				
-				System.out.println("1st sub task^^^^^^^^^^^^^^ "+methods[i].getSubs()[0].subtasks[0]);
+				System.out.println("1st sub task^^^^^^^^^^^^^^ "+methods[i].getLabel(0));
+				for(int j=0;j<methods[i].getSubs()[0].subtasks.length;j++)
+				{
+					System.out.print(j+"-"+methods[i].getSubs()[0].subtasks[j]+"; ");
+					
+				}
+				System.out.println("");
 			}
 		}
 		else if(task.getHead().isVar())
